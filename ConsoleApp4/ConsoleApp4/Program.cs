9b08c5b13060bc43b745d449a45562f10f61fa14
Adisirinli <islamea@code.edu.az>
2023-10-15 00:10:22 +0400

    arrayler ve flowcart

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,56 @@
+﻿#region task1
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int resoult = 0;
+//foreach (int number in numbers)
+//{
+//    resoult += number;
+
+//}
+//Console.WriteLine(resoult);
+#endregion
+#region task2
+//int[] numbers = { 1, 2, 3, 60, 59, 30, 20, 13, 12, 10 };
+//int resault = 0;
+//foreach (int number in numbers)
+//{
+//    if (number >= 10)
+//    {
+//        resault++;
+//    }
+//}
+//Console.WriteLine(resault);
+
+
+#endregion
+#region task3
+using System.Diagnostics.CodeAnalysis;
+
+//int[] numbers = { 1, 2, 3, 60, 59, 30, 20, 13, 12, 10 };
+//int resault = 0;
+//foreach (int number in numbers)
+//{
+//    resault += number;
+
+//}
+//Console.WriteLine(resault/numbers.Length);
+
+#endregion
+#region task4
+
+//string[] aylar = { "yanvar", "fevral", "mart", "aprel", "may", "iyun", "iyul", "avqust", "sentyabr", "oktyabr", "noyabr", "dekabr" };
+
+//foreach (string ay in aylar)
+//{
+//    if (ay == "mart" || ay == "iyun" || ay == "iyul")
+//    {
+//        Console.WriteLine($"{ay} yay ayidir");
+//    }
+
+//}
+
+
+
+
+
+
+#endregion
